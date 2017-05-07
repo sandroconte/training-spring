fd39805aeb5e39ab5c294b89e518415ee8c87d4d
sandro conte <sandroconte@MacBook-Pro-di-sandro.local>
2017-05-08 01:28:54 +0200

    play with random

--- src/two/training/services/implementations/RandomFortune.java
+++ src/two/training/services/implementations/RandomFortune.java
@@ -8,11 +8,20 @@ import two.training.services.Fortune;
 
 @Component
 public class RandomFortune implements Fortune {
-
+	
+	private String[] data = {
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward",
+			"This is your lucky day!",
+			"It will better next time"
+	};
+	
+	private Random random = new Random();
+	
 	@Override
-	public String getFortune() {	
-		Random random = new Random();
-		return random.nextBoolean() ? "This is your lucky day!" : "It will better next time";
+	public String getFortune() {			
+		return data[random.nextInt(data.length)];
 	}
 
 }
