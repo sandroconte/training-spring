deeb42a951322952e5da04c8c42f9da69d7c9daa
sandro conte <sandroconte@MacBook-Pro-di-sandro.local>
2017-05-11 01:36:08 +0200

    scope component

--- src/two/training/app/AnnotaionDemoApp.java
+++ src/two/training/app/AnnotaionDemoApp.java
@@ -5,15 +5,18 @@ import java.util.Random;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import two.training.services.Coach;
+import two.training.services.implementations.TennisCoach;
 
 public class AnnotaionDemoApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		TennisCoach theCoach = context.getBean("tennisCoach", TennisCoach.class);
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmail());
+		System.out.println(theCoach.getTeam());
 		context.close();
 	}
 
--- src/two/training/services/implementations/TennisCoach.java
+++ src/two/training/services/implementations/TennisCoach.java
@@ -2,17 +2,36 @@ package two.training.services.implementations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import two.training.services.Coach;
 import two.training.services.Fortune;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
 	@Qualifier("randomFortune")
 	private Fortune fortune;
+	
+	@Value("${team}")
+	private String team;
+
+	@Value("${email}")
+	private String email;
+
+	public String getTeam() {
+		return team;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
 	
 	public TennisCoach(){}
 	
